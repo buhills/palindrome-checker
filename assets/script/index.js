585ae7aa40f8a688f380f7c130cd2494ba36582f
4 Rebase Academy <4rebaseacademy@iMac-de-4.local>
2023-12-06 21:44:08 +0100

    linters erros fixed

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,21 +1,21 @@
 // Check point for inputed string
 function palindromeText (str) {
   let j = str.length - 1
-  for(let i = 0; i < j/2; i++) {
-    if(str[i] !== str[j]) {
+  for (let i = 0; i < j / 2; i++) {
+    if (str[i] !== str[j]) {
       return false
-    } 
+    }
     j--
   }
   return true
 }
 
-function check_palindrome() {
+function checkPalindrome () {
   // Get the input value from the text field
-  const inputText = document.getElementById ('input_text').value
+  const inputText = document.getElementById('input_text').value
   // Checking if it is a palindrome
-  const isPalindrome = palindromeText (inputText)
-  const resultElement = document.getElementById ('result')
+  const isPalindrome = palindromeText(inputText)
+  const resultElement = document.getElementById('result')
   if (isPalindrome) {
     resultElement.innerHTML = 'Yes, input text is a palindrome.'
   } else {
